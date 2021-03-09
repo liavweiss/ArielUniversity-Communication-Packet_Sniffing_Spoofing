0e16f537ce41fdb1a27086642e063ed5b11f29ab
liavweiss <74174727+liavweiss@users.noreply.github.com>
2021-03-09 14:00:24 +0200

    Delete task 1 1 B ICMP.py

--- task 1 1 B ICMP.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from scapy.all import *
-def print_pkt(pkt):
-  pkt.show()
-  
-print('sniffing packets...')
-pkt = sniff(iface=['br-e7b7a0685397', 'enp0s3'], filter='icmp', prn=print_pkt)
